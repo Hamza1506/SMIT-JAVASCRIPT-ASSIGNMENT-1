686d6b9c2ac0546f66b04cd571d684b51799a9f2
Hamza1506 <hamzapervez397@gmail.com>
2023-09-01 23:30:44 +0500

    JS push 1

---
+++ index.js
@@ -0,0 +1,54 @@
+//Chapter 1
+
+//Question 1
+alert("Assalam-o-Alaikum")
+
+//Question 2
+alert("Error! Please enter a valid password.")
+
+//Question 3
+alert("Wellcome to JS land. \n Happy Coding!")
+
+//Question 4
+alert("Wellcome to JS Land..")
+alert("Happy Coding!")
+
+
+//-----------------------------------------------------------------------
+
+//Chapter 2
+
+//Question 1
+var username;
+//Question 2
+var myName="Muhammad Hamza Pervez"
+//Question 3
+var message="Hello World"
+alert(message)
+
+//Question 4
+alert("John Doe")
+alert("15 year old")
+alert("Certified Mobile App development")
+
+// //Question 5
+var message = "Pizza \n Pizz \n Piz \n Pi \n P";
+alert(message);
+
+
+// //Question 6
+
+var email="hamzapervez397@gmail.com"
+alert("My email address is "+ email)
+
+
+// //Question 7
+var book="A smart way to learn JavaScript"
+alert("I'm trying to learn from the book "+ book)
+
+// //Question 8
+
+document.write("Yeah i can write html content through JS")
+
+// //Question 9
+alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
